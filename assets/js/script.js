a584f04fc75d5188a511adeb449178417e7e53e5
razaqultegar <myrazaqul@gmail.com>
2024-04-02 06:23:01 +0700

    Refactoring

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,6 @@
 jQuery(function ($) {
 	"use strict";
 
-	$( '.product-slider .slick-slider' ).slick({
-		infinite: false,
-		slidesToShow: 6,
-		slidesToScroll: 6,
-	});
-
 	$( '.channel-content .slick-slider' ).slick({
 		infinite: false,
 		slidesToShow: 4,
--- woocommerce/global/breadcrumb.php
+++ woocommerce/global/breadcrumb.php
@@ -8,26 +8,31 @@
 
 if ( ! empty( $breadcrumb ) ) {
 
-?>
+	echo '<nav class="qa-breadcrumb hidden my-6 md:block" aria-label="breadcrumbs">
+		<ul class="text-neutral-600 text-xs font-bold leading-4 flex gap-1">';
 
-<nav class="qa-breadcrumb hidden lg:inline-block mt-6" aria-label="Breadcrumb">
-	<ul class="text-neutral-600 text-xs font-bold flex gap-1">
+	foreach ( $breadcrumb as $key => $crumb ) {
 
-	<?php foreach ( $breadcrumb as $key => $crumb ) { ?>
-		<?php if ( ! empty( $crumb[1] ) && sizeof( $breadcrumb ) !== $key + 1 ) { ?>
-			<li>
-				<a href="<?php echo esc_url( $crumb[1] ) ?>"><?php echo esc_html( $crumb[0] ) ?></a>
-			</li>
-		<?php } else { ?>
-			<li class="flex gap-1 items-center">
+		if ( ! empty( $crumb[1] ) && sizeof( $breadcrumb ) !== $key + 1 ) {
+			if ( $key == 0 ) {
+				echo '<li>
+					<a href="' . esc_url( $crumb[1] ) . '">' . esc_html( $crumb[0] ) . '</a>
+				</li>';
+			} else {
+				echo '<li class="flex gap-1 items-center">
+					<span class="di-chevron-right"></span>
+					<a href="' . esc_url( $crumb[1] ) . '">' . esc_html( $crumb[0] ) . '</a>
+				</li>';
+			}
+		} else {
+			echo '<li class="flex gap-1 items-center">
 				<span class="di-chevron-right"></span>
-				<a href="<?php echo esc_url( $crumb[1] ) ?>"><?php echo esc_html( $crumb[0] ) ?></a>
-			</li>
-		<?php } ?>
-	<?php } ?>
+				<span class="text-neutral-300">' . esc_html( $crumb[0] ) . '</span>
+			</li>';
+		}
 
-	</ul>
-</nav>
+	}
 
-<?php
+	echo '</ul>
+	</nav>';
 }
