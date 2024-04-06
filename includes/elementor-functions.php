caac39718e1e9b96e422f6a8db6da2244ffc3562
razaqultegar <myrazaqul@gmail.com>
2024-04-06 09:46:00 +0700

    Add Banner Carousel Elementor Widgets

--- includes/elementor-functions.php
+++ includes/elementor-functions.php
@@ -22,9 +22,11 @@ add_action( 'elementor/elements/categories_registered', 'dots_elementor_register
 // Register Elementor widgets.
 function dots_elementor_register_widget( $widgets_manager ) {
 	require_once DOTS_THEME_DIR . '/includes/elementor/banner-slides.php';
+	require_once DOTS_THEME_DIR . '/includes/elementor/banner-carousel.php';
 	require_once DOTS_THEME_DIR . '/includes/elementor/brand-carousel.php';
 
 	$widgets_manager->register( new \Elementor_Banner_Slides_Widget() );
+	$widgets_manager->register( new \Elementor_Banner_Carousel_Widget() );
 	$widgets_manager->register( new \Elementor_Brand_Carousel_Widget() );
 }
 add_action( 'elementor/widgets/register', 'dots_elementor_register_widget' );
@@ -32,6 +34,7 @@ add_action( 'elementor/widgets/register', 'dots_elementor_register_widget' );
 // Register styles and scripts for Elementor Widgets.
 function dots_elementor_widgets_dependencies() {
 	wp_register_script( 'dea-banner-slides-script', DOTS_THEME_URI . 'assets/js/elementor/banner-slides.js', array( 'elementor-frontend' ), DOTS_THEME_VERSION, true );
+	wp_register_script( 'dea-banner-carousel-script', DOTS_THEME_URI . 'assets/js/elementor/banner-carousel.js', array( 'elementor-frontend' ), DOTS_THEME_VERSION, true );
 	wp_register_script( 'dea-brand-carousel-script', DOTS_THEME_URI . 'assets/js/elementor/brand-carousel.js', array( 'elementor-frontend' ), DOTS_THEME_VERSION, true );
 }
 add_action( 'wp_enqueue_scripts', 'dots_elementor_widgets_dependencies' );
